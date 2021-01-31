d6fcb8f341f5b433f34ade5b5661ec73cadce85b
Karan Dhillon <me@kdhillon.com>
2021-01-31 15:12:58 -0500

    [feature] Add top bar and back button for player details screen

--- app/src/main/java/dev/johnoreilly/fantasypremierleague/ui/players/playerDetails/PlayerDetailsView.kt
+++ app/src/main/java/dev/johnoreilly/fantasypremierleague/ui/players/playerDetails/PlayerDetailsView.kt
@@ -1,8 +1,9 @@
 package dev.johnoreilly.fantasypremierleague.ui.players.playerDetails
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,42 +17,63 @@ import dev.johnoreilly.fantasypremierleague.ui.players.PlayersViewModel
 @Composable
 fun PlayerDetailsView(
     player: Player,
-    playersViewModel: PlayersViewModel
+    playersViewModel: PlayersViewModel,
+    popBackStack: () -> Unit
 ) {
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth()
-    ) {
-        Row(
-            modifier = Modifier
-                .preferredHeight(150.dp)
-                .fillMaxWidth()
-        ) {
-            CoilImage(
-                data = player.photoUrl,
-                modifier = Modifier.preferredSize(150.dp),
-                contentDescription = player.name
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Player details")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { popBackStack() }) {
+                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                    }
+                }
             )
+        },
+        bodyContent = {
             Column(
-                verticalArrangement = Arrangement.Center,
                 modifier = Modifier
-                    .preferredHeight(100.dp)
+                    .padding(16.dp)
                     .fillMaxWidth()
             ) {
-                Text(player.name, style = MaterialTheme.typography.h6)
-                Text(player.team, style = MaterialTheme.typography.subtitle1, color = Color.DarkGray)
+                Row(
+                    modifier = Modifier
+                        .preferredHeight(150.dp)
+                        .fillMaxWidth()
+                ) {
+                    CoilImage(
+                        data = player.photoUrl,
+                        modifier = Modifier.preferredSize(150.dp),
+                        contentDescription = player.name
+                    )
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .preferredHeight(100.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Text(player.name, style = MaterialTheme.typography.h6)
+                        Text(
+                            player.team,
+                            style = MaterialTheme.typography.subtitle1,
+                            color = Color.DarkGray
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.size(16.dp))
+                Row {
+                    Text(text = "POINTS:", style = MaterialTheme.typography.subtitle1)
+                    Text(
+                        player.points.toString(),
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                        style = MaterialTheme.typography.subtitle1,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
             }
         }
-        Spacer(modifier = Modifier.size(16.dp))
-        Row {
-            Text(text = "POINTS:", style = MaterialTheme.typography.subtitle1)
-            Text(
-                player.points.toString(),
-                modifier = Modifier.align(Alignment.CenterVertically),
-                style = MaterialTheme.typography.subtitle1,
-                fontWeight = FontWeight.Bold
-            )
-        }
-    }
+    )
 }
