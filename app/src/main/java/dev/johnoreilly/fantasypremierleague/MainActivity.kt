a885c129f2c5a442137b401ffa821620fbcea744
Karan Dhillon <me@kdhillon.com>
2021-02-01 05:17:44 -0500

    [Feature] Setup Bottom navigation bar globally
    * Primary destinations are 'Player detials' and 'Fixtures' for now.

--- app/src/main/java/dev/johnoreilly/fantasypremierleague/MainActivity.kt
+++ app/src/main/java/dev/johnoreilly/fantasypremierleague/MainActivity.kt
@@ -2,11 +2,20 @@ package dev.johnoreilly.fantasypremierleague
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
 import androidx.navigation.NavType
 import androidx.navigation.compose.*
 import dev.johnoreilly.fantasypremierleague.ui.Screen
+import dev.johnoreilly.fantasypremierleague.ui.bottomNavigationItems
 import dev.johnoreilly.fantasypremierleague.ui.global.FantasyPremierLeagueTheme
 import dev.johnoreilly.fantasypremierleague.ui.players.PlayersViewModel
 import dev.johnoreilly.fantasypremierleague.ui.fixtures.FixturesListView
@@ -32,31 +41,58 @@ fun MainLayout(playersViewModel: PlayersViewModel) {
     val navController = rememberNavController()
 
     FantasyPremierLeagueTheme {
-        NavHost(navController, startDestination = Screen.PlayerListScreen.title) {
-            composable(Screen.PlayerListScreen.title) {
-                PlayerListView(
-                    playersViewModel = playersViewModel,
-                    onPlayerSelected = { playerId ->
-                        navController.navigate(Screen.PlayerDetailsScreen.title + "/${playerId}")
-                    }
-                )
-            }
-            composable(
-                Screen.PlayerDetailsScreen.title + "/{playerId}",
-                arguments = listOf(navArgument("playerId") { type = NavType.IntType })
-            ) { navBackStackEntry ->
-                val playerId: Int? = navBackStackEntry.arguments?.getInt("playerId")
-                val player = playersViewModel.players.value?.first { it.id == playerId }
+        Scaffold(
+            bottomBar = {
+                BottomNavigation {
+                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+                    val currentRoute = navBackStackEntry?.arguments?.getString(KEY_ROUTE)
 
-                // TODO error handling for invalid playerId edge case or when first throws an exception
-                PlayerDetailsView(
-                    player = player!!,
-                    playersViewModel = playersViewModel,
-                    popBackStack = { navController.popBackStack() }
-                )
+                    bottomNavigationItems.forEach { bottomNavigationitem ->
+                        BottomNavigationItem(
+                            icon = {
+                                Icon(
+                                    bottomNavigationitem.icon,
+                                    contentDescription = bottomNavigationitem.iconContentDescription
+                                )
+                            },
+                            selected = currentRoute == bottomNavigationitem.route,
+                            onClick = {
+                                navController.navigate(bottomNavigationitem.route) {
+                                    popUpTo = navController.graph.startDestination
+                                    launchSingleTop = true
+                                }
+                            }
+                        )
+                    }
+                }
             }
-            composable(Screen.FixtureListScreen.title) {
-                FixturesListView(playersViewModel = playersViewModel)
+        ) {
+            NavHost(navController, startDestination = Screen.PlayerListScreen.title) {
+                composable(Screen.PlayerListScreen.title) {
+                    PlayerListView(
+                        playersViewModel = playersViewModel,
+                        onPlayerSelected = { playerId ->
+                            navController.navigate(Screen.PlayerDetailsScreen.title + "/${playerId}")
+                        }
+                    )
+                }
+                composable(
+                    Screen.PlayerDetailsScreen.title + "/{playerId}",
+                    arguments = listOf(navArgument("playerId") { type = NavType.IntType })
+                ) { navBackStackEntry ->
+                    val playerId: Int? = navBackStackEntry.arguments?.getInt("playerId")
+                    val player = playersViewModel.players.value?.first { it.id == playerId }
+
+                    // TODO error handling for invalid playerId edge case or when first throws an exception
+                    PlayerDetailsView(
+                        player = player!!,
+                        playersViewModel = playersViewModel,
+                        popBackStack = { navController.popBackStack() }
+                    )
+                }
+                composable(Screen.FixtureListScreen.title) {
+                    FixturesListView(playersViewModel = playersViewModel)
+                }
             }
         }
     }
--- app/src/main/java/dev/johnoreilly/fantasypremierleague/ui/Screen.kt
+++
@@ -1,7 +0,0 @@
-package dev.johnoreilly.fantasypremierleague.ui
-
-sealed class Screen(val title: String) {
-    object PlayerListScreen : Screen("PlayerListScreen")
-    object PlayerDetailsScreen : Screen("PlayerDetailsScreen")
-    object FixtureListScreen : Screen("FixtureListScreen")
-}
