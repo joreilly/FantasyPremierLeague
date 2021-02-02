1efd08717ddcf55fc2c06ea1d688fff8dcb80301
Karan Dhillon <me@kdhillon.com>
2021-02-02 18:03:31 -0500

    [feature] Setup basic skeleton for FixturesListView and FixtureView

--- common/src/commonMain/kotlin/dev/johnoreilly/common/repository/FantasyPremierLeagueRepository.kt
+++ common/src/commonMain/kotlin/dev/johnoreilly/common/repository/FantasyPremierLeagueRepository.kt
@@ -1,19 +1,15 @@
 package dev.johnoreilly.common.repository
 
+import dev.johnoreilly.common.entities.GameFixture
+import dev.johnoreilly.common.entities.Player
+import dev.johnoreilly.common.getLogger
 import dev.johnoreilly.common.model.BootstrapStaticInfo
 import dev.johnoreilly.common.remote.FantasyPremierLeagueApi
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 
-data class Player(val id: Int, val name: String, val team: String, val photoUrl: String,
-                  val points: Int, val currentPrice: Double, val goalsScored: Int, val assists: Int)
-
-
-data class GameFixture(val id: Int, val kickoffTime: String, val homeTeam: String, val awayTeam: String)
-
-class FantasyPremierLeagueRepository  : KoinComponent {
+class FantasyPremierLeagueRepository : KoinComponent {
     private val fantasyPremierLeagueApi: FantasyPremierLeagueApi by inject()
-
     private var bootstrapStaticInfo: BootstrapStaticInfo? = null
 
     suspend fun getBootstrapStaticInfo(): BootstrapStaticInfo {
@@ -33,15 +29,17 @@ class FantasyPremierLeagueRepository  : KoinComponent {
         return bootstrapStaticInfo.elements
             .sortedByDescending { it.total_points }
             .map { player ->
-            val playerName = "${player.first_name} ${player.second_name}"
-            val playerImageUrl = "https://resources.premierleague.com/premierleague/photos/players/110x140/p${player.code}.png"
-            val team = bootstrapStaticInfo.teams.find { it.code == player.team_code }
-            val teamName = team?.name ?: ""
-            val currentPrice = player.now_cost/10.0
+                val playerName = "${player.first_name} ${player.second_name}"
+                val playerImageUrl = "https://resources.premierleague.com/premierleague/photos/players/110x140/p${player.code}.png"
+                val team = bootstrapStaticInfo.teams.find { it.code == player.team_code }
+                val teamName = team?.name ?: ""
+                val currentPrice = player.now_cost / 10.0
 
-            Player(player.id, playerName, teamName, playerImageUrl,
-                player.total_points, currentPrice, player.goals_scored, player.assists)
-        }
+                Player(
+                    player.id, playerName, teamName, playerImageUrl,
+                    player.total_points, currentPrice, player.goals_scored, player.assists
+                )
+            }
     }
 
     @Throws(Exception::class)
@@ -49,9 +47,23 @@ class FantasyPremierLeagueRepository  : KoinComponent {
         val bootstrapStaticInfo = getBootstrapStaticInfo()
 
         return fetchFixtures().map { fixture ->
-            val homeTeam = bootstrapStaticInfo.teams[fixture.team_h-1].name
-            val awayTeam = bootstrapStaticInfo.teams[fixture.team_a-1].name
-            GameFixture(fixture.id, fixture.kickoff_time ?: "", homeTeam, awayTeam)
+            val homeTeam = bootstrapStaticInfo.teams[fixture.team_h-1]
+            val homeTeamName = homeTeam.name
+            val homeTeamCode = homeTeam.code
+            val homeTeamPhotoUrl = "https://resources.premierleague.com/premierleague/badges/t${homeTeamCode}.png"
+
+            val awayTeamCode = bootstrapStaticInfo.teams[fixture.team_a-1].code
+            val awayTeamName = bootstrapStaticInfo.teams[fixture.team_a-1].name
+            val awayTeamPhotoUrl = "https://resources.premierleague.com/premierleague/badges/t${awayTeamCode}.png"
+
+            GameFixture(
+                fixture.id,
+                fixture.kickoff_time ?: "",
+                homeTeamName,
+                awayTeamName,
+                homeTeamPhotoUrl,
+                awayTeamPhotoUrl
+            )
         }
     }
 }
