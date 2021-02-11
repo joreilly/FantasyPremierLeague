6e8fc5a17c625414e51fa4608f04066dbd1316d3
Karan Dhillon <me@kdhillon.com>
2021-02-11 16:41:52 -0500

    [feature] Improve FixtureDetailsView to have the same UI PlayerDetailsView

--- app/src/main/java/dev/johnoreilly/fantasypremierleague/presentation/fixtures/fixtureDetails/FixtureDetailsView.kt
+++ app/src/main/java/dev/johnoreilly/fantasypremierleague/presentation/fixtures/fixtureDetails/FixtureDetailsView.kt
@@ -8,9 +8,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import dev.johnoreilly.common.data.model.GameSettingsDto
+import androidx.compose.ui.unit.sp
+import dev.johnoreilly.fantasypremierleague.presentation.fixtures.ClubInFixtureView
 import dev.johnoreilly.fantasypremierleague.presentation.players.PlayersViewModel
 
 @Composable
@@ -35,15 +37,80 @@ fun FixtureDetailsView(
             )
         },
         bodyContent = {
-            Column(modifier = Modifier.fillMaxSize()) {
-                Spacer(modifier = Modifier.size(16.dp))
-                Text(
-                    text = "${pastFixtureAsState.value?.homeTeam} VS ${pastFixtureAsState.value?.awayTeam} ",
-                    style = MaterialTheme.typography.h6,
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
-                )
-                Spacer(modifier = Modifier.size(16.dp))
+            pastFixtureAsState.value?.let { fixture ->
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        ClubInFixtureView(
+                            fixture.homeTeam,
+                            fixture.homeTeamPhotoUrl
+                        )
+                        Text(
+                            text = "(${fixture.homeTeamScore})",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 25.sp,
+                            color = MaterialTheme.colors.onSurface
+                        )
+                        Text(
+                            text = "vs",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 25.sp
+                        )
+                        Text(
+                            text = "(${fixture.awayTeamScore})",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 25.sp,
+                            color = MaterialTheme.colors.onSurface
+                        )
+                        ClubInFixtureView(
+                            fixture.awayTeam,
+                            fixture.awayTeamPhotoUrl
+                        )
+                    }
+
+                    val formattedTime = "%02d:%02d".format(
+                        fixture.localKickoffTime.hour,
+                        fixture.localKickoffTime.minute
+                    )
+                    PastFixtureStatView(statName = "Date", statValue = fixture.localKickoffTime.date.toString())
+                    PastFixtureStatView(statName = "Kick Off Time", statValue = formattedTime)
+                }
             }
         }
     )
+}
+
+@Composable
+fun PastFixtureStatView(statName: String, statValue: String) {
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                Text(
+                    text = statName,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Column {
+                Text(
+                    text = statValue,
+                    color = Color(0xFF3179EA),
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+        Divider(thickness = 1.dp)
+    }
 }
