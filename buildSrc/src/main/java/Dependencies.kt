cf9cd44434e53017334e6cef60769d4f80e9953b
John O'Reilly <oreillyj@gmail.com>
2021-05-05 19:42:48 +0100

    compose beta6, accompanist 0.9.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -9,9 +9,9 @@ object Versions {
     const val kotlinxDateTime = "0.1.1"
 
     const val slf4j = "1.7.30"
-    const val compose = "1.0.0-beta05"
+    const val compose = "1.0.0-beta06"
     const val nav_compose = "1.0.0-alpha10"
-    const val accompanist = "0.8.0"
+    const val accompanist = "0.9.0"
 
     const val junit = "4.13"
 }
