765c9515a3f2370071070924288dce7081580470
John O'Reilly <oreillyj@gmail.com>
2021-10-23 19:09:04 +0100

    jvm code updates

--- common/src/jvmMain/kotlin/dev/johnoreilly/common/Main.kt
+++ common/src/jvmMain/kotlin/dev/johnoreilly/common/Main.kt
@@ -3,7 +3,6 @@ package dev.johnoreilly.common
 import dev.johnoreilly.common.data.remote.FantasyPremierLeagueApi
 import dev.johnoreilly.common.di.initKoin
 import kotlinx.datetime.*
-import kotlin.time.Duration
 
 suspend fun main() {
     val koin = initKoin(enableNetworkLogs = true).koin
@@ -12,28 +11,40 @@ suspend fun main() {
     val staticInfo = api.fetchBootstrapStaticInfo()
     val fixtures = api.fetchFixtures()
 
+    
+    println("Positions")
+    staticInfo.element_types.forEach {
+        println("${it.id}: ${it.singular_name_short}")
+    }
 
-    val now = Clock.System.now()
-    fixtures.forEach { fixture ->
 
-        fixture.kickoff_time?.let { kickoffTime ->
-            val durationTillKickOff: Duration = kickoffTime - now
-            if (durationTillKickOff.inWholeSeconds > 0 && durationTillKickOff.inWholeDays < 7) {
-                val datetimeInSystemZone: LocalDateTime = kickoffTime.toLocalDateTime(TimeZone.currentSystemDefault())
+    println("Events")
+    staticInfo.events.forEach {
+        println("${it.id}, ${it.name}")
+    }
 
-                val homeTeam = staticInfo.teams.find { it.id == fixture.team_h }
-                val awayTeam = staticInfo.teams.find { it.id == fixture.team_a }
-                if (homeTeam != null && awayTeam != null) {
-                    println("$datetimeInSystemZone ${fixture.team_h_difficulty}:${fixture.team_a_difficulty}  $durationTillKickOff ${fixture.kickoff_time}: ${homeTeam.name} vs ${awayTeam.name}")
-                }
-            }
+    val currentEvent = staticInfo.events.find { it.is_current == true }
+    val nextEvent = staticInfo.events.find { it.is_next == true }
+
+    println("next game week fixtures")
+    val nextFixtures = fixtures.filter { it.event == nextEvent?.id }
+    nextFixtures.forEach { fixture ->
+
+        val datetimeInSystemZone = fixture.kickoff_time?.toLocalDateTime(TimeZone.currentSystemDefault())
+
+        val homeTeam = staticInfo.teams.find { it.id == fixture.team_h }
+        val awayTeam = staticInfo.teams.find { it.id == fixture.team_a }
+        if (homeTeam != null && awayTeam != null) {
+            println("${fixture.event}: $datetimeInSystemZone ${fixture.team_h_difficulty}:${fixture.team_a_difficulty}: ${homeTeam.name} vs ${awayTeam.name}")
         }
     }
 
     val players = staticInfo.elements
-    println(players.size)
-
-    staticInfo.elements.sortedByDescending { it.total_points }.take(5).forEach { element ->
-        println("${element.first_name} ${element.second_name}: ${element.now_cost/10.0} ${element.total_points}")
+    println("Players")
+    players.sortedByDescending { it.total_points }.take(10).forEach { element ->
+        val position = staticInfo.element_types.find { it.id == element.element_type }
+        println("${element.first_name} ${element.second_name}: ${element.now_cost/10.0} ${element.total_points} ${position?.singular_name_short}")
     }
+
+
 }
