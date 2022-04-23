b095f17c3d3afb5e6085c1c0c7757238852e045a
John O'Reilly <oreillyj@gmail.com>
2022-04-23 22:39:51 +0100

    Kotlin 1.6.20 + related updates

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,19 +1,20 @@
 
 object Versions {
-    const val kotlin = "1.6.10"
-    const val kotlinCoroutines = "1.6.0-native-mt"
-    const val ktor = "1.6.7"
+    const val kotlin = "1.6.20"
+    const val kotlinCoroutines = "1.6.1"
+    const val ktor = "2.0.0"
     const val kotlinxSerialization = "1.3.1"
-    const val koin = "3.1.4"
+    const val koin = "3.1.6"
     const val realm = "0.10.2"
     const val kermit = "1.0.0"
-    const val kotlinxDateTime = "0.3.1"
+    const val kotlinxDateTime = "0.3.2"
 
     const val slf4j = "1.7.30"
-    const val compose = "1.1.0"
-    const val composeCompiler = "1.1.0"
-    const val navCompose = "2.4.1"
+    const val compose = "1.2.0-alpha08"
+    const val composeCompiler = "1.2.0-alpha08"
+    const val navCompose = "2.4.2"
     const val coilCompose = "1.4.0"
+    const val composeDesktop = "0.0.0-master-dev673"
 
     const val junit = "4.13"
 }
@@ -26,9 +27,36 @@ object AndroidSdk {
 }
 
 object Deps {
-    const val kermit = "co.touchlab:kermit:${Versions.kermit}"
-    const val kotlinxDateTime = "org.jetbrains.kotlinx:kotlinx-datetime:${Versions.kotlinxDateTime}"
     const val realm = "io.realm.kotlin:library-base:${Versions.realm}"
+    const val kermit = "co.touchlab:kermit:${Versions.kermit}"
+
+    object Kotlinx {
+        const val serializationCore = "org.jetbrains.kotlinx:kotlinx-serialization-core:${Versions.kotlinxSerialization}"
+        const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.kotlinCoroutines}"
+        const val dateTime = "org.jetbrains.kotlinx:kotlinx-datetime:${Versions.kotlinxDateTime}"
+    }
+
+    object Koin {
+        val core = "io.insert-koin:koin-core:${Versions.koin}"
+        val test = "io.insert-koin:koin-test:${Versions.koin}"
+        val android = "io.insert-koin:koin-android:${Versions.koin}"
+        val compose = "io.insert-koin:koin-androidx-compose:${Versions.koin}"
+    }
+
+    object Ktor {
+        const val clientCore = "io.ktor:ktor-client-core:${Versions.ktor}"
+        const val clientJson = "io.ktor:ktor-client-json:${Versions.ktor}"
+        const val clientLogging = "io.ktor:ktor-client-logging:${Versions.ktor}"
+        const val clientSerialization = "io.ktor:ktor-client-serialization:${Versions.ktor}"
+        const val contentNegotiation = "io.ktor:ktor-client-content-negotiation:${Versions.ktor}"
+        const val json = "io.ktor:ktor-serialization-kotlinx-json:${Versions.ktor}"
+
+        const val clientAndroid = "io.ktor:ktor-client-android:${Versions.ktor}"
+        const val clientJava = "io.ktor:ktor-client-java:${Versions.ktor}"
+        const val slf4j = "org.slf4j:slf4j-simple:${Versions.slf4j}"
+        const val clientIos = "io.ktor:ktor-client-ios:${Versions.ktor}"
+    }
+
 }
 
 object Test {
@@ -47,28 +75,6 @@ object Compose {
     const val coilCompose = "io.coil-kt:coil-compose:${Versions.coilCompose}"
 }
 
-object Koin {
-    val core = "io.insert-koin:koin-core:${Versions.koin}"
-    val test = "io.insert-koin:koin-test:${Versions.koin}"
-    val android = "io.insert-koin:koin-android:${Versions.koin}"
-    val compose = "io.insert-koin:koin-androidx-compose:${Versions.koin}"
-}
 
-object Ktor {
-    val clientCore = "io.ktor:ktor-client-core:${Versions.ktor}"
-    val clientJson = "io.ktor:ktor-client-json:${Versions.ktor}"
-    val clientLogging = "io.ktor:ktor-client-logging:${Versions.ktor}"
-    val clientSerialization = "io.ktor:ktor-client-serialization:${Versions.ktor}"
-
-    val clientAndroid = "io.ktor:ktor-client-android:${Versions.ktor}"
-    val clientApache = "io.ktor:ktor-client-apache:${Versions.ktor}"
-    val clientJava = "io.ktor:ktor-client-java:${Versions.ktor}"
-    val slf4j = "org.slf4j:slf4j-simple:${Versions.slf4j}"
-    val clientIos = "io.ktor:ktor-client-ios:${Versions.ktor}"
-}
-
-object Serialization {
-    val core = "org.jetbrains.kotlinx:kotlinx-serialization-core:${Versions.kotlinxSerialization}"
-}
 
 
--- common/common.podspec
+++ common/common.podspec
@@ -2,24 +2,20 @@ Pod::Spec.new do |spec|
     spec.name                     = 'common'
     spec.version                  = '1.0'
     spec.homepage                 = 'Link to a Kotlin/Native module homepage'
-    spec.source                   = { :git => "Not Published", :tag => "Cocoapods/#{spec.name}/#{spec.version}" }
+    spec.source                   = { :http=> ''}
     spec.authors                  = ''
     spec.license                  = ''
     spec.summary                  = 'Fantasy Football Premier League'
-
-    spec.vendored_frameworks      = "build/cocoapods/framework/FantasyPremierLeagueKit.framework"
-    spec.libraries                = "c++"
-    spec.module_name              = "#{spec.name}_umbrella"
-
+    spec.vendored_frameworks      = 'build/cocoapods/framework/FantasyPremierLeagueKit.framework'
+    spec.libraries                = 'c++'
                 
-
                 
-
+                
     spec.pod_target_xcconfig = {
         'KOTLIN_PROJECT_PATH' => ':common',
-        'PRODUCT_MODULE_NAME' => 'common',
+        'PRODUCT_MODULE_NAME' => 'FantasyPremierLeagueKit',
     }
-
+                
     spec.script_phases = [
         {
             :name => 'Build common',
@@ -35,8 +31,9 @@ Pod::Spec.new do |spec|
                 "$REPO_ROOT/../gradlew" -p "$REPO_ROOT" $KOTLIN_PROJECT_PATH:syncFramework \
                     -Pkotlin.native.cocoapods.platform=$PLATFORM_NAME \
                     -Pkotlin.native.cocoapods.archs="$ARCHS" \
-                    -Pkotlin.native.cocoapods.configuration=$CONFIGURATION
+                    -Pkotlin.native.cocoapods.configuration="$CONFIGURATION"
             SCRIPT
         }
     ]
+                
 end
