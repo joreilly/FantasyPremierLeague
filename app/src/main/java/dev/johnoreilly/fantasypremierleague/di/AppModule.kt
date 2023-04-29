78b267ce09e1ff8fe4c1d20baa71b919a7a149ab
John O'Reilly <oreillyj@gmail.com>
2023-04-29 19:07:32 +0100

    fixturess view model koin updates

--- app/src/main/java/dev/johnoreilly/fantasypremierleague/di/AppModule.kt
+++ app/src/main/java/dev/johnoreilly/fantasypremierleague/di/AppModule.kt
@@ -7,5 +7,5 @@ import org.koin.dsl.module
 
 val appModule = module {
     viewModel { FantasyPremierLeagueViewModel(get()) }
-    viewModel { FixturesViewModel() }
+    viewModel { FixturesViewModel(get()) }
 }
--- app/src/main/java/dev/johnoreilly/fantasypremierleague/presentation/fixtures/FixturesViewModel.kt
+++ app/src/main/java/dev/johnoreilly/fantasypremierleague/presentation/fixtures/FixturesViewModel.kt
@@ -3,12 +3,9 @@ package dev.johnoreilly.fantasypremierleague.presentation.fixtures
 import androidx.lifecycle.ViewModel
 import dev.johnoreilly.common.data.repository.FantasyPremierLeagueRepository
 import kotlinx.coroutines.flow.StateFlow
-import org.koin.java.KoinJavaComponent.inject
 
-class FixturesViewModel: ViewModel() {
 
-    private val repository: FantasyPremierLeagueRepository by inject(FantasyPremierLeagueRepository::class.java)
-
+class FixturesViewModel(private val repository: FantasyPremierLeagueRepository): ViewModel() {
     val gameweekToFixtures = repository.gameweekToFixtures
 
     val currentGameweek: StateFlow<Int>
