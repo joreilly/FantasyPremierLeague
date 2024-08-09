9285c647accf9aedb3e409ac62432ac52366ec7a
Márton Braun <marton.braun@jetbrains.com>
2024-08-09 14:50:17 +0200

    Update Room version to 2.7.0-alpha06

--- common/src/commonMain/kotlin/dev/johnoreilly/common/database/AppDatabase.kt
+++ common/src/commonMain/kotlin/dev/johnoreilly/common/database/AppDatabase.kt
@@ -1,22 +1,18 @@
 package dev.johnoreilly.common.database
 
-import androidx.room.Database
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverter
-import androidx.room.TypeConverters
+import androidx.room.*
 import dev.johnoreilly.common.model.GameFixture
 import dev.johnoreilly.common.model.Player
 import dev.johnoreilly.common.model.Team
 import kotlinx.datetime.LocalDateTime
 
-@Database(entities = [Team::class, Player::class, GameFixture::class], version = 1)
-@TypeConverters(LocalDateTimeConverter::class)
-abstract class AppDatabase : RoomDatabase(), DB {
-    abstract fun fantasyPremierLeagueDao(): FantasyPremierLeagueDao
+internal expect object AppDatabaseCtor : RoomDatabaseConstructor<AppDatabase>
 
-    override fun clearAllTables() {
-        super.clearAllTables()
-    }
+@Database(entities = [Team::class, Player::class, GameFixture::class], version = 1)
+@ConstructedBy(AppDatabaseCtor::class)
+@TypeConverters(LocalDateTimeConverter::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun fantasyPremierLeagueDao(): FantasyPremierLeagueDao
 }
 
 internal const val dbFileName = "fantasypremierleague.db"
@@ -32,11 +28,4 @@ class LocalDateTimeConverter {
     fun dateToTimestamp(date: LocalDateTime?): String? {
         return date?.toString()
     }
-}
-
-
-// FIXME: Added a hack to resolve below issue:
-// Class 'AppDatabase_Impl' is not abstract and does not implement abstract base class member 'clearAllTables'.
-interface DB {
-    fun clearAllTables(): Unit {}
 }
