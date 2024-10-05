24a70266665bd6e43bfa982a499701f26fe4b556
Kanake <10kanake@gmail.com>
2024-10-05 03:31:45 +0300

    use named `iconContentDescription` for better readability.

--- app/src/main/java/dev/johnoreilly/fantasypremierleague/presentation/ScreenAndBottomNavigationItems.kt
+++ app/src/main/java/dev/johnoreilly/fantasypremierleague/presentation/ScreenAndBottomNavigationItems.kt
@@ -26,19 +26,19 @@ val bottomNavigationItems = listOf(
     BottomNavigationitem(
         Screen.PlayerListScreen.title,
         Icons.Default.Person,
-        "Player",
+        iconContentDescription = "Player",
         label = "Players"
     ),
     BottomNavigationitem(
         Screen.FixtureListScreen.title,
         Icons.Filled.DateRange,
-        "Fixtures",
+        iconContentDescription = "Fixtures",
         label = "Fixtures"
     ),
     BottomNavigationitem(
         Screen.LeagueStandingsListScreen.title,
         Icons.Filled.List,
-        "League",
+        iconContentDescription = "League",
         label = "League"
     )
 )
