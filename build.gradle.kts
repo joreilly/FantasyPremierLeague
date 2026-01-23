5e99b6858a0e2baa813583a2b5173cb578704b45
Vinodh Raj R <vinodhrraj@gmail.com>
2026-01-23 17:59:36 +0530

    added detekt setup with custom compose rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,29 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinx.serialization) apply false
+    alias(libs.plugins.detekt) apply false
     //alias(libs.plugins.skie) apply false
+}
+
+
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("org.jetbrains.kotlin.jvm") ||
+            plugins.hasPlugin("org.jetbrains.kotlin.android") ||
+            plugins.hasPlugin("org.jetbrains.kotlin.multiplatform")) {
+            apply(plugin = "dev.detekt")
+
+            extensions.configure<dev.detekt.gradle.extensions.DetektExtension> {
+                debug = false
+                allRules = false
+                disableDefaultRuleSets = true
+                buildUponDefaultConfig = false
+                baseline.set(file("baseline.xml"))
+            }
+
+            dependencies {
+                "detektPlugins"("dev.uitest:compose-rules:0.1.4.13")
+            }
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        mavenLocal()
         //maven("https://maven.pkg.jetbrains.space/public/p/compose/dev/")
     }
 }
